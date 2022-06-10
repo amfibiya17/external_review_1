01451ce360ca56dc2b3e9dac1ffb03ad1b007729
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-06-10 22:24:10 +0100

    TDD simple test for uncounted color

--- counter.js
+++ counter.js
@@ -4,8 +4,10 @@ function counter(string) {
     return "Red: 1";
   } else if(string === "Green") {
     return "Green: 1";
-  } else {
+  } else if(string === "Amber") {
     return "Amber: 1";
+  } else {
+    return "Uncounted: 1";
   }
   
 }
