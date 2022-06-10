e0825fb48ca52d4185326e62cbee820dbfd42ba9
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-06-10 16:30:47 +0100

    second simple test for red and amber

--- counter.test.js
+++ counter.test.js
@@ -4,4 +4,8 @@ describe('counter', () => {
   it('returns "Red: 1" when input is "Red" ', () => {
     expect(counter("Red")).toEqual("Red: 1");
   });
+
+  it('returns "Amber: 1\nRed: 1" when input is "Amber, Red" ', () => {
+    expect(counter("Amber, Red")).toEqual("Amber: 1\nRed: 1");
+  });
 });
